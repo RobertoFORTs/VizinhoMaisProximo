be3755b283e3523cf8486a80ff1c1dcd2bb6b4a5
RobertoFORTs <bobfortesneto@gmail.com>
2023-05-16 15:48:51 -0400

    Implementando lógica da arvore KD  por etapas

--- vizinhoProximo.c
+++ vizinhoProximo.c
@@ -14,19 +14,33 @@ void kd_build(tree *ptree, float (*compara)(const void *a, const void *b)){
   ptree -> compara = compara;
 }
 void kd_insert(tree *ptree, void *pdata, int depth){
-  node **ppNode;
-  node *pNode;
+  tree **ppNode;
+  tree *pNode;
   ppNode = &(ptree->root);
   pNode = *ppNode;
   depth = 0;
+  ptree->compara = compare;
+  struct Kd_Node* pDataResultant = (struct Kd_Node*)pdata;
+
   while (pNode != NULL){
     
     if (depth%2==0){
-      //realizar comparação a partir da coordenada x
+      if (compare(pNode->root->coordinate[0] < pDataResultant->coordinate[0]) < 0){
+        //mover para esquerda
+      }
+      else{
+        //mover para direita
+      }
     }
     else{
-      //realizar comparação a partir da coordenada y
+      if (compare(pNode->root->coordinate[1] < pDataResultant->coordinate[1]) < 0){
+        //mover para esquerda
+      }
+      else{
+        //mover para direita
+      }
     }
 
+    depth++;
   }
 }
